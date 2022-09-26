a2fcf86d1b8e149939c96f8fa34fb764f113e705
jheron <j.e.z.paco@gmmail.com>
2022-09-26 14:04:39 -0400

    sis

--- SIS457.cpp
+++ SIS457.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 
 int main(){
-    cout<<"holas";
-system("pause");
-    return 0;
+	int a;
+	
+	do {
+		cout<<"Yo , Elvis Jherson Zurita Paco estudiante de la carrera de sistemas \n me conprometo a dar tod mi esfuerso en pasar la materia \n";
+		cout<<"Ingrese 1 para vover a ver el mensaje \nIngrese 2 para finalisar\n";
+		cin>>a;
+	}while(!(a==2));
+	//system("pause");
+	return 0;
 }
